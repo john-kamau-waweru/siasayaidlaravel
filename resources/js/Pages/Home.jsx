f5d12490d5d1cfafad4cd87657077829acc70ef9
root <root@NDK205-JohnWaweru-win.localdomain>
2025-03-28 16:49:32 +0300

    updates

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -40,13 +40,22 @@ export default function Home() {
           </p>
 
           <div className="mt-6 flex flex-col md:flex-row md:flex-wrap gap-2">
-            <Link className="inline-flex items-center justify-center text-sm bg-[#ED1D24] text-white px-4 py-3 rounded-full" href="/submit">
+            <Link
+              className="inline-flex items-center justify-center gap-2 text-sm border border-[#ED1D24] shadow bg-[#ED1D24] hover:bg-white hover:text-[#ED1D24] text-white font-medium rounded-full px-4 py-3"
+              href="/submit"
+            >
               What did you like? 👍🏾
             </Link>
-            <Link className="inline-flex items-center justify-center text-sm bg-black text-white px-4 py-3 rounded-full" href="/submit">
+            <Link
+              className="inline-flex items-center justify-center gap-2 text-sm border border-black shadow bg-black hover:bg-white hover:text-black text-white font-medium rounded-full px-4 py-3"
+              href="/submit"
+            >
               What didn’t you like? 👎🏾
             </Link>
-            <Link className="inline-flex items-center justify-center text-sm bg-white border border-[#166E38] text-[#166E38] px-4 py-3 rounded-full" href="/experiences">
+            <Link
+              className="inline-flex items-center justify-center gap-2 text-sm border border-[#166E38] shadow bg-white hover:bg-[#166E38] hover:text-white text-[#166E38] font-medium rounded-full px-4 py-3"
+              href="/experiences"
+            >
               Hear from others 📢
             </Link>
           </div>
@@ -56,8 +65,120 @@ export default function Home() {
         </div>
       </section>
 
+      {/* Three Cards Section */}
+      <section className="p-8 py-16 bg-gray-50 w-full">
+        <div className="max-w-[80%] mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
+          {[
+            {
+              title: "Security",
+              text: "Your ID ensures your security and access to essential services.",
+              icon: (
+                <FaShieldAlt className="text-[#ED1D24] text-4xl mb-4 mx-auto animate-fadeIn" />
+              ),
+            },
+            {
+              title: "Verification",
+              text: "A national ID helps verify identity and prevent fraud.",
+              icon: (
+                <FaCheckCircle className="text-[#166E38] text-4xl mb-4 mx-auto animate-fadeIn" />
+              ),
+            },
+            {
+              title: "Accessibility",
+              text: "An ID opens doors to education, healthcare, and voting rights.",
+              icon: (
+                <FaGlobe className="text-black text-4xl mb-4 mx-auto animate-fadeIn" />
+              ),
+            },
+          ].map(({ title, text, icon }) => (
+            <div
+              key={title}
+              className="p-6 bg-white shadow-md rounded-md text-center flex flex-col items-center transform transition-transform duration-300 hover:scale-105"
+            >
+              {icon}
+              <h3 className="text-gray-900 text-2xl font-medium">{title}</h3>
+              <p className="mt-2 text-gray-700">{text}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+
       {/* Experience Slider */}
       <ExperiencesSlider />
+
+      {/* CTA */}
+      <section className="w-full flex flex-col items-center text-center px-6 md:px-8 py-8 md:py-8 bg-[#166E38]">
+        <div className="max-w-[80%] mx-auto">
+          {/* Content */}
+          <div className="flex justify-center mb-4">
+            <Link href="/">
+              <img
+                src="/images/logo2.jpg"
+                width="120"
+                height="auto"
+                alt="siasa place"
+                className="rounded-lg"
+              />
+            </Link>
+          </div>
+          <p className="text-white max-w-2xl text-sm md:text-base">
+            Access to IDs is a major barrier to young people's political
+            participation.
+          </p>
+          <p className="text-white mb-6 md:mb-10 max-w-2xl text-sm md:text-base">
+            Follow Siasa Place on social media to stay informed and be part of
+            the movement for change!
+          </p>
+
+          {/* Social Media Icons */}
+          <div className="flex justify-center gap-6">
+            <a
+              href="https://www.facebook.com/siasaplace/?ref=br_rs"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF className="text-white w-6 h-6 hover:scale-110 hover:opacity-80 transition-transform" />
+            </a>
+            <a
+              href="https://twitter.com/siasaplace"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter className="text-white w-6 h-6 hover:scale-110 hover:opacity-80 transition-transform" />
+            </a>
+            <a
+              href="https://ke.linkedin.com/in/siasa-place-414327167"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedinIn className="text-white w-6 h-6 hover:scale-110 hover:opacity-80 transition-transform" />
+            </a>
+            <a
+              href="https://www.youtube.com/@siasaplacekenya"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaYoutube className="text-white w-6 h-6 hover:scale-110 hover:opacity-80 transition-transform" />
+            </a>
+            <a
+              href="https://www.instagram.com/siasaplaceke"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram className="text-white w-6 h-6 hover:scale-110 hover:opacity-80 transition-transform" />
+            </a>
+            <a
+              href="https://www.tiktok.com/@siasaplace"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTiktok className="text-white w-6 h-6 hover:scale-110 hover:opacity-80 transition-transform" />
+            </a>
+          </div>
+        </div>
+      </section>
+
+      
 
       {/* Footer */}
       <footer className="p-8 text-center bg-gray-50 w-full mx-auto">
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { createInertiaApp } from "@inertiajs/inertia-react";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers"
+import { createInertiaApp } from '@inertiajs/react'
+import { createRoot } from 'react-dom/client'
 import '../css/app.css';
 
 createInertiaApp({
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`,import.meta.glob('./Pages/**/*.jsx')),
-   setup({el, App, props}){
+  resolve: name => {
+    const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
+    return pages[`./Pages/${name}.jsx`]
+  },
+  setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />)
-   },
+  },
 })
